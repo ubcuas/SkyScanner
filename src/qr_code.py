9ab51e66897c992188aa6e8b3f0bf27195d511df
Aden Chan <adenchan@gmail.com>
2022-10-15 15:08:05 -0700

    :building_construction: moved some stuff into functions so i can read it better

--- src/qr_code.py
+++ src/qr_code.py
@@ -20,61 +20,10 @@ args = ap.parse_args()
 def main():
     # for loaded sample input images
     if args.image:
-        image = cv2.imread(args.image)
-        barcodes = decode(image)  # detect and decode barcodes
-        if barcodes is not None:
-            for barcode in barcodes:
-                (x, y, w, h) = barcode.rect
-                cv2.rectangle(image, (x, y), (x + w, y + h),
-                              (0, 0, 255), 2)  # bounding box
-                barcodeData = barcode.data.decode("utf-8")  # convert to string
-                barcodeType = barcode.type
-                # barcode data and barcode type on string img
-                text = "{} ({})".format(barcodeData, barcodeType)
-                cv2.putText(image, text, (x, y - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-                print("[INFO] Found {}:\n{}".format(barcodeType, barcodeData))
-                sendToGCOM(barcodeData)
-        else:
-            print("[INFO] No QR Code found")
-
+        scanImage()
     # for camera scanned images
     elif args.camera:
-
-        barcodeDataLast = None
-        # open cv video camera capture
-        window = cv2.namedWindow("Camera Scan Index {}".format(args.camera))
-        cap = cv2.VideoCapture(int(args.camera))
-
-        while True:
-            ret, im = cap.read()
-            if not ret:
-                continue
-
-            # Read Image
-            size = im.shape
-            gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY, dstCn=0)
-            image = decode(gray)
-
-            for barcode in image:
-                (x, y, w, h) = barcode.rect
-                cv2.rectangle(im, (x, y), (x + w, y + h),
-                              (0, 0, 255), 2)  # bounding box
-                barcodeData = barcode.data.decode("utf-8")  # convert to string
-                barcodeType = barcode.type
-                if barcodeData != barcodeDataLast:
-                    print("[INFO] Found {}:\n{}".format(
-                        barcodeType, barcodeData))
-                    sendToGCOM(barcodeData)
-                barcodeDataLast = barcodeData
-
-            # Display image
-            cv2.imshow("Camera Scan Index {}".format(args.camera), im)
-            
-             # Check if window was closed
-            if cv2.waitKey(1) and cv2.getWindowProperty("Camera Scan Index {}".format(args.camera), cv2.WND_PROP_VISIBLE) != 1:
-                break
-        cv2.destroyAllWindows()
+        runCameraScanner()
 
 def sendToGCOM(data):
     try:
@@ -84,6 +33,61 @@ def sendToGCOM(data):
         return
     except Exception as e:
         print(e)
-    
+
+def scanImage():
+    image = cv2.imread(args.image)
+    barcodes = decode(image)  # detect and decode barcodes
+    if barcodes is not None:
+        for barcode in barcodes:
+            (x, y, w, h) = barcode.rect
+            cv2.rectangle(image, (x, y), (x + w, y + h),
+                            (0, 0, 255), 2)  # bounding box
+            barcodeData = barcode.data.decode("utf-8")  # convert to string
+            barcodeType = barcode.type
+            # barcode data and barcode type on string img
+            text = "{} ({})".format(barcodeData, barcodeType)
+            cv2.putText(image, text, (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            print("[INFO] Found {}:\n{}".format(barcodeType, barcodeData))
+            sendToGCOM(barcodeData)
+    else:
+        print("[INFO] No QR Code found")
+
+def runCameraScanner():
+    barcodeDataLast = None
+    # open cv video camera capture
+    window = cv2.namedWindow("Camera Scan Index {}".format(args.camera))
+    cap = cv2.VideoCapture(int(args.camera))
+
+    while True:
+        ret, im = cap.read()
+        if not ret:
+            continue
+
+        # Read Image
+        size = im.shape
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY, dstCn=0)
+        image = decode(gray)
+
+        for barcode in image:
+            (x, y, w, h) = barcode.rect
+            cv2.rectangle(im, (x, y), (x + w, y + h),
+                            (0, 0, 255), 2)  # bounding box
+            barcodeData = barcode.data.decode("utf-8")  # convert to string
+            barcodeType = barcode.type
+            if barcodeData != barcodeDataLast:
+                print("[INFO] Found {}:\n{}".format(
+                    barcodeType, barcodeData))
+                sendToGCOM(barcodeData)
+            barcodeDataLast = barcodeData
+
+        # Display image
+        cv2.imshow("Camera Scan Index {}".format(args.camera), im)
+        
+            # Check if window was closed
+        if cv2.waitKey(1) and cv2.getWindowProperty("Camera Scan Index {}".format(args.camera), cv2.WND_PROP_VISIBLE) != 1:
+            break
+    cv2.destroyAllWindows()
+
 if __name__ == '__main__':
     main()
